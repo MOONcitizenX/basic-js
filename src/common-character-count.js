7d71d0a170bafb6cf3d9d13f27a51a688e070c4a
MOONcitizenX <mooncitizenx@gmail.com>
2022-10-08 11:09:12 +0300

    feat: finish count-cats and common-character-count tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+const getCommonCharacterCount = (s1, s2) => {
+	let count = 0;
+	const str1 = s1.split('');
+	const str2 = s2.split('');
+	let maxLines = Math.max(s1.length, s2.length);
+	for (let i = 0; i < maxLines; i++) {
+		if (str1) {
+			const charOut1 = str1.pop();
+			if (str2.includes(charOut1)) {
+				const charOut2 = str2.splice(
+					str2.findIndex((char) => char === charOut1),
+					1
+				);
+				count++;
+			}
+		}
+	}
+	return count;
+};
 
 module.exports = {
-  getCommonCharacterCount
+	getCommonCharacterCount
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+const countCats = (matrix) => {
+	let cats = 0;
+	matrix.forEach((el) => (cats += el.filter((item) => item === '^^').length));
+	return cats;
+};
 
 module.exports = {
-  countCats
+	countCats
 };
